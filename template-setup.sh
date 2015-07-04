eca1118111874b23c464884687a631127f341b1e
Michael Bond <mbond@the-forgotten.org>
2015-07-04 13:37:05 -0400

    initial commit for template setup

---
+++ template-setup.sh
@@ -0,0 +1,10 @@
+#!/bin.bash
+
+# @TODO : these should be put in a separate file and sourced from hydra/template-setup.sh
+
+PROJECT_NAME="holt"
+BASE_DIR="/vagrant"
+PROJECT_DIR="$BASE_DIR/$PROJECT_NAME"
+GITDIR="/tmp/git"
+GIT_REPO="hydraSetup"
+REPO_DIR="$GITDIR/$GIT_REPO"
