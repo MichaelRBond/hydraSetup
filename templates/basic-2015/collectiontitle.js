79464c25ccce0eaaf27a9d23c76efad5f9dfd42a
Tim Broadwater <emailme@timbroadwater.com>
2015-08-03 14:40:31 -0400

    Smooth Scrolling JS

--- templates/basic-2015/collectiontitle.js
+++ templates/basic-2015/collectiontitle.js
@@ -133,3 +133,19 @@ $(function () {
 		$("#search-toggle #search-toggle2").addClass("hiding");			
 	});
 });
+
+// Smooth Scrolling for Anchors
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
