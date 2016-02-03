982f0cc05e1edbbe2a75e2646c8ae6e48815a13c
Tim Broadwater <emailme@timbroadwater.com>
2016-02-03 15:01:07 -0500

    Mobile Homepage Help Modal

--- templates/basic-2015/collectiontitle.js
+++ templates/basic-2015/collectiontitle.js
@@ -198,3 +198,27 @@ $(window).load(function() {
 $(window).resize(function(){
   equalheight('.document');
 });
+
+// Homepage Help Modal Cookie
+$(function () {
+  // set the cookie 
+  var myCookie = "ModalShown"; 
+  var cookieValue = getCookieValue(myCookie); 
+  console.log(myCookie.length);
+  
+  if(cookieValue.length === 0){
+    // $('#openModal').modal('show'); 
+  } else { 
+    $('#openModal').toggleClass("destroy");
+  }
+  
+    $("#openModalbut").click(function () {            
+      $("#openModal").toggleClass("destroy");
+      document.cookie = "ModalShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+    });
+ 
+});
+function getCookieValue(a, b) {
+    b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
+    return b ? b.pop() : '';
+}
